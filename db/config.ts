01c26c97f806c962dc07264b91fbd6075c5c4dde
John Harte <56019064+john-harte@users.noreply.github.com>
2025-12-09 14:44:55 +0000

    Revert "Team glasgow trains updates to search"

--- db/config.ts
+++ db/config.ts
@@ -14,34 +14,6 @@ const Article = defineTable({
   },
 });
 
-const RailTrip = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true }),
-    origin: column.text(),
-    destination: column.text(),
-    departureDateTime: column.date(),
-    arrivalDateTime: column.date(),
-    carrier: column.text(),
-    duration: column.number(),
-    stops: column.text(),
-    price: column.number(),
-    hasPowerSockets: column.boolean(),
-    hasWifi: column.boolean(),
-    wifiReliability: column.number({ optional: true }),
-    punctuality: column.number(),
-  },
-});
-
-const SeatClass = defineTable({
-  columns: {
-    id: column.number({ primaryKey: true }),
-    tripId: column.number({ references: () => RailTrip.columns.id }),
-    className: column.text(),
-    description: column.text(),
-    priceModifier: column.number(),
-  },
-});
-
 export default defineDb({
-  tables: { Article, RailTrip, SeatClass },
+  tables: { Article },
 })
